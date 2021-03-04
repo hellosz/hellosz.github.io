371cabfb399e77a870539df318843ab9409a2e08
neng.tian <neng.tian@patpat.com>
2021-03-04 23:37:16 +0800

    dep new project

--- start.sh
+++ start.sh
@@ -5,6 +5,6 @@ HOST_IP=$(ifconfig $VAR | grep "broadcast" | awk '{ print $2}')
 # echo $HOST_IP
 
 # start jekyll serve
-bundle exec jekyll serve --watch --trace --host=$HOST_IP --incremental
+bundle exec jekyll serve --detach --port=9001
 
 
