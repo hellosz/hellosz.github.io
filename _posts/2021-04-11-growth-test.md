3434df25815965debd99f7c77b8dbde053b925b1
neng.tian <neng.tian@patpat.com>
2021-04-21 10:15:38 +0800

    feat module:增加growth模块

---
+++ _posts/2021-04-11-growth-test.md
@@ -0,0 +1,17 @@
+---
+layout: article
+tags: growth fitness diary
+category: growth 
+title:  成长日记 
+mode: immersive
+cover: 
+header:
+  theme: dark
+article_header:
+  type: cover
+  image:
+	  src: /assets/headers/street_workout_1280x583.jpg
+comment: true
+key: 2fd3c5fb131a841168ca078a055dbc6ce1bdhaaa
+---
+
--- start.sh
+++ start.sh
@@ -1,8 +1,11 @@
 #! /bin/bash
 # 获取本机IP
-VAR=$(awk '{print $2}' ./.env)
-HOST_IP=$(ifconfig $VAR | grep "broadcast" | awk '{ print $2}')
+# VAR=$(awk '{print $2}' ./.env)
+# HOST_IP=$(ifconfig $VAR | grep "broadcast" | awk '{ print $2}')
 # echo $HOST_IP
+
+# build
+bundle exec jekyll build
 
 # 删除之前的启动脚本
 sudo pkill bundle
